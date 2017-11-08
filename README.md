cebf6efadcc8387fc5e1b5b682da2efc184b4a81
brandonhon <brandonhon@gmail.com>
2017-11-08 16:18:48 -0600

    more updates (:

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
 # Oh-My-Psh
 
-Instructions coming soon. 
+#### NOTE: This is a work in progress (WIP). Use at your own risk.
+
+My version of [Oh-My-Zsh](ohmyz.sh) for Powershell v4+ based soley on the work of Gonzalo Peci and his work
+[Oh-My-Posh](https://github.com/pecigonzalo/Oh-My-Posh). Thank you Gonzalo for the hardwork and the inspiration.
+
+# Installation Instructions
+
+Requirements:
+* Git - Needed for installing from internet.
+* Powershell 4+ (not tested on previous versions)
+
+
+### Internet Installation
+Run:
+
+```powershell
+cd ~
+iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/pecigonzalo/Oh-My-Posh/master/install.ps1'))
+```
+
+Add the following line to your powershell profile:
+```powershell
+Import-Module "Oh-My-Posh" -DisableNameChecking -NoClobber
+```
+Now reload your Powershell profile
+
+### Alternative Installation (local installation):
+Download and extract or clone the repository into a folder e.g. ```C:\TEMP```
+Open a Powershell session and run
+```powershell
+cd C:\TEMP
+.\install.ps1 -local $true
+```
+
+# Configuration
+
+Configuration parameters are found under
+```powershell
+$env:USERPROFILE\.powershellrc.ps1
+```
+Open it with your prefered editor and change as you want, keep in mind some functionality is still WIP but don't hesitate on reporting any issue you have.
--- install.ps1
+++ install.ps1
@@ -10,31 +10,45 @@ function Install-OMP {
   Check-Recommends
   Check-Dependencies
 
-  Write-Output "Deleting $Env:USERPROFILE\.oh-my-posh"
-  Remove-Item -Force -Recurse "$Env:USERPROFILE\.oh-my-posh" -ErrorAction SilentlyContinue
+  Write-Output "Deleting $Env:USERPROFILE\.oh-my-sph"
+  Remove-Item -Force -Recurse "$Env:USERPROFILE\.oh-my-psh" -ErrorAction SilentlyContinue
   if ($local) {
     # Deploy from current folder
-    Write-Output "Coping Oh-My-Posh to its destination"
-    Copy-Item -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-posh\"
+    Write-Output "Coping Oh-My-Psh to its destination"
+    Copy-Item -Recurse -Force .\  "$Env:USERPROFILE\.oh-my-psh\"
   } else {
     # Clone project
-    Write-Output "Cloning Oh-My-Posh from Github"
-    git clone https://github.com/pecigonzalo/Oh-My-Posh.git $Env:USERPROFILE\.oh-my-posh
+    Write-Output "Cloning Oh-My-Psh from Github"
+    git clone https://github.com/pecigonzalo/Oh-My-Psh.git $Env:USERPROFILE\.oh-my-psh
   }
   # Copy module to the user modules folder
-  Write-Output "Installting Oh-My-Posh Module"
+  Write-Output "Installting Oh-My-Psh Module"
   New-Item -Type Directory "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules" -Force | Out-Null
-  Copy-Item -Recurse -Force $Env:USERPROFILE\.oh-my-posh\modules\oh-my-posh  `
+  Copy-Item -Recurse -Force $Env:USERPROFILE\.oh-my-psh\modules\oh-my-psh  `
     "$([Environment]::GetFolderPath("mydocuments"))\WindowsPowerShell\Modules\"
+}
+
+# Add Oh-My-Psh to local Powershell profile
+function Insert-Content {
+  param ( [String]$Path )
+  process {
+    $( ,$_; Get-Content $Path -ea SilentlyContinue) | Out-File $Path
+  }
+}
+
+if (! ( Test-Path $PROFILE ) ) {
+  Write-Output 'No profile found. Please create profile by running "New-Item -Path $PROFILE -Type File -Force" in the console.'
+} else {
+  "#`r`n" + '# Welcome to a whole new world of Powershell script management.' + "`r`n#`r`n" + 'Import-Module "Oh-My-Psh" -DisableNameChecking -NoClobber' + "`r`n`r`n" | Insert-Content $PROFILE
 }
 
 #
 # Install logic
 #
-if ( Test-Path $Env:USERPROFILE\.oh-my-posh ) {
-  Write-Output "Oh-My-Posh is already installed"
+if ( Test-Path $Env:USERPROFILE\.oh-my-psh ) {
+  Write-Output "Oh-My-Psh is already installed"
   if ( $force -eq $true ) {
-    Write-Output "Reinstalling Oh-My-Posh"
+    Write-Output "Reinstalling Oh-My-Psh"
     Install-OMP
   }
 } else {
