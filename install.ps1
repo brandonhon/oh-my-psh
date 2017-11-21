47ada9fb75a89f28e513eb78ddbf5c3901d1ff30
brandonhon <brandonhon@gmail.com>
2017-11-21 15:53:16 -0600

    n/a

--- install.ps1
+++ install.ps1
@@ -3,13 +3,31 @@ param (
   [bool]$force = $false
 )
 
-# Load utils
-. ./tools/utils.ps1
+function Check-Dependencies {
+  $exit = 0
+  # Check git installed
+  try {
+    Get-Command git -ErrorAction Stop | Out-Null
+  } catch {
+    Write-Error "Git not found, please install git or add it to your PATH before running again"
+    $exit = 1
+  }
+  # If any errors exit install
+  if ($exit) { exit 1 }
+}
+
+function Check-Recommends {
+  # Check PSCX installed
+  if (!(Get-Module PSCX -ListAvailable)) {
+    Write-Warning "PSCX not found, while not required it is recommended: https://www.powershellgallery.com/packages/Pscx/"
+  }
+  # Check PSColor installed
+  if (!(Get-Module PSColor -ListAvailable)) {
+    Write-Warning "PSColor not found, while not required it is recommended: https://github.com/pecigonzalo/pscolor"
+  }
+}
 
 function Install-OMP {
-  Check-Recommends
-  Check-Dependencies
-
   Write-Output "Deleting $Env:USERPROFILE\.oh-my-psh"
   Remove-Item -Force -Recurse "$Env:USERPROFILE\.oh-my-psh" -ErrorAction SilentlyContinue
   if ($local) {
@@ -55,9 +73,6 @@ if ( Test-Path $Env:USERPROFILE\.oh-my-psh ) {
 } else {
   Install-OMP
 }
-. $PROFILE
-
-
 
 # Check for profile and create/prepend as necessary
 if (! ( Test-Path $PROFILE ) ) {
@@ -82,3 +97,5 @@ if (! ( Test-Path $PROFILE ) ) {
   Write-Host "Enjoy!" -ForegroundColor Green
   Write-Host "`r`n"
 }
+
+. $PROFILE
